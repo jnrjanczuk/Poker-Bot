6147f6ead9c2eead9e419ef8028ac8be32612290
jnrjanczuk <jack.janczuk@gmail.com>
2026-04-29 21:54:06 -0400

    implemented deal function

--- poker.cpp
+++ poker.cpp
@@ -1,6 +1,7 @@
 #include <vector>
 #include <algorithm>
 #include <random>
+#include <queue>
 using namespace std;
 
 // Enum Classes
@@ -24,8 +25,9 @@ struct Card {
 }; // Card
 
 struct Player {
-    pair<Card, Card> hand;
+    vector<Card> hand;
     double chipCount;
+    bool CPU;
 }; // Player
 
 struct Deck {
@@ -42,9 +44,11 @@ struct Deck {
     }
 }; // Deck
 
-struct Game {
-
-}; // Game
+struct Hand {
+    vector<Player> players;
+    queue<Card> cards;
+    int bigBlind;
+}; // Hand
 
 // Helper Functions
 
@@ -54,6 +58,39 @@ void shuffle(Deck& deck) {
     static mt19937 gen(rd);
     shuffle(deck.cards.begin(), deck.cards.end(), gen);
 } // shuffle
+
+// returns index of next player
+int next(int pos, int n) {
+    return (pos + 1) % n;
+}
+
+// returns index of previous player
+int prev(int pos, int n) {
+    return (pos - 1) % n;
+}
+
+void deal(Hand& hand, Deck& deck) {
+    // clear players' previous hands
+    for (auto& player : hand.players) {
+        player.hand.clear();
+    }
+    
+    // shuffle deck
+    shuffle(deck);
+
+    // deal cards to players (starting with small blind)
+    int n = hand.players.size();
+    int smallBlind = prev(hand.bigBlind, n);
+    // outer loop to deal around twice
+    for (int i = 0; i < 2; i++) {
+        // inner loop to deal to each player
+        for (int j = 0; j < n; j++) {
+            int index = (smallBlind + i) % n;
+            hand.players[index].hand.emplace_back(deck.cards.back());
+            deck.cards.pop_back();
+        }
+    }
+} // deal
 
 int main() {
     return 0;
